c4e7ba804a799b91972bab5cd99e72e9c70248c4
70734 <bohicks96@gmail.com>
2024-06-16 17:02:18 -0500

    Create Sketch Box grid using js
    Outerloop creates each row of the Box
    Innerloop fills each row with elements
    This makes the complete the complete box

---
+++ script.js
@@ -0,0 +1,19 @@
+let sketchContainer = document.querySelector('.sketch-container');
+let blockSize = 16;
+
+// create sketch box based on blockSize
+function createSketchBox(){
+    for(let i = 0; i < blockSize; i++){ //loop to create rows for etch a sketch
+        let blockRow = document.createElement('div');
+        sketchContainer.appendChild(blockRow);
+        blockRow.classList.toggle('block-row');
+
+    
+        for(let j = 0; j < blockSize; j++) // fill row with block elements
+            {
+                let block = document.createElement('div');
+                blockRow.appendChild(block);
+                block.classList.toggle('block');
+            }
+    }
+}
