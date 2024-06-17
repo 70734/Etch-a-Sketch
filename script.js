b3591351605837329e4f76516101bee5f3a08a0c
70734 <bohicks96@gmail.com>
2024-06-17 15:42:44 -0500

    Add button to allow user to choice box size
    Function created to clear box so when the user can pick size multiple times
    Function created to get user input to decide the size
    Button created in html and style added

--- script.js
+++ script.js
@@ -1,21 +1,18 @@
-let sketchContainer = document.querySelector('.sketch-container');
-let blockSize = 16;
-let color = 'pink';
 
-function drawOnSketch(block){
+function drawOnSketch(block, color='pink'){
     block.style.backgroundColor = color;
 }
 
 
 // create sketch box based on blockSize
-function createSketchBox(){
-    for(let i = 0; i < blockSize; i++){ //loop to create rows for etch a sketch
+function createSketchBox(size){
+    for(let i = 0; i < size; i++){ //loop to create rows for etch a sketch
         let blockRow = document.createElement('div');
         sketchContainer.appendChild(blockRow);
         blockRow.classList.toggle('block-row');
 
     
-        for(let j = 0; j < blockSize; j++) // fill row with block elements
+        for(let j = 0; j < size; j++) // fill row with block elements
             {
                 let block = document.createElement('div');
                 blockRow.appendChild(block);
@@ -25,4 +22,31 @@ function createSketchBox(){
     }
 }
 
-createSketchBox();
+
+function removeSketchBox(size){ // remove grid from sketch box
+    box = document.querySelectorAll('.block');
+    boxRow = document.querySelectorAll('.block-row');
+
+    box.forEach((element) => element.remove());
+    boxRow.forEach((element) => element.remove());
+}
+function setUpSketch(){ // set up etch a sketch with entered value
+    removeSketchBox();
+    let go = true;
+    let blockSize = 16;
+    while(go){
+        blockSize = prompt("Enter Size of box");
+        blockSize = Number(blockSize);
+        if(!Number.isInteger(blockSize) || blockSize < 1 || blockSize >= 100){
+            alert("Error: try again");
+        }
+        else{
+            go = false;
+        }
+    }
+    createSketchBox(blockSize);
+}
+
+let sketchContainer = document.querySelector('.sketch-container');
+let button = document.querySelector('button');
+button.addEventListener('click', () => setUpSketch());
